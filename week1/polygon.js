643de468c7fb75863c88166d3a0026ad7ba79af3
yeonkyujeon <ehb6915@naver.com>
2020-11-03 17:31:32 +0900

    다각형 js 리팩토링 완료

--- week1/polygon.js
+++ week1/polygon.js
@@ -1,71 +1,53 @@
-let order = '';
-let printResult = '';
 
-function printExecutionSequence() {
-    console.log('계산수행순서 : ' + order.substring(2,order.length));
-    console.log(printResult);
-}
-
-function getCircle(radius, amount) {
-    if (amount === undefined) {
-        const circleArea = Math.pow(radius, 2) * Math.PI;
-        console.log('circleArea(원의넓이) = ' + circleArea);
-        order+= ", circle";
-        printResult += `circleArea(원의넓이) = ${circleArea} \n`;
-    } else {
-        if (radius <= amount) {
-            let sum = 0;
-            let circleAreaSum = 0;
-            for (let i = radius; i <= amount; i++) {
-                sum = Math.pow(i, 2) * Math.PI;
-                circleAreaSum += sum;
-            }
-            console.log('circleAreaSum(원의넓의합) = ' + circleAreaSum);
-            order+= ", circle";
-            printResult += `circleAreaSum(원의넓의합) =  ${circleAreaSum} \n`;
-        }else{
-            console.log('원의 갯수를 잘못입력하셨습니다');
-            order+= ", circle";
-            printResult += '원의 갯수를 잘못 입력함 \n';
+class Polygon {
+    constructor() {
+        this.executionSequence = [];
+    }
+    getCircle(radius, amount = radius) {
+        let sum = 0;
+        while (radius <= amount) {
+            sum += Math.pow(radius, 2) * Math.PI;
+            radius++;
         }
+        this.addRecord('circle', sum);
     }
 
-}
-function getRect(length, height) {
-    const rectArea = length * height;
-    console.log('rectArea(사각형넓이) = ' + rectArea);
-    order+= ", rect";
-    printResult += `rectArea(사각형넓이) =  ${rectArea} \n`;
-}
-function getTrapezoid(upperSide, lowerSide, height) {
-    const trapezoidArea = ((upperSide + lowerSide) * height) / 2;
-    console.log('trapezoidArea(사다리꼴넓이) = ' + trapezoidArea);
-    order+= ", trapezoid";
-    printResult += `trapezoidArea(사다리꼴넓이) =  ${trapezoidArea} \n`;
-}
-
-function getArea(name, arg1, arg2, arg3) {
-
-    switch (name) {
-        case 'circle':
-            getCircle(arg1, arg2);
-            break;
-        case 'rect':
-            getRect(arg1, arg2);
-            break;
-        case 'trapezoid':
-            getTrapezoid(arg1, arg2, arg3);
-            break;
-
-        default:
-            console.log('잘못입력하셨습니다');
-            
+    getRect(length, height){
+        this.addRecord('rect', length*height);
     }
-}
 
-getCircle(2);
-getCircle(2,4);
-getArea('circle',2);
-getArea('rect',2,3);
-getArea('trapezoid', 10,15,12);
-printExecutionSequence() ;
+    getTrapezoid(upperSide, lowerSide, height){
+        this.addRecord('trapezoid', (upperSide + lowerSide) * height / 2);
+    }
+
+    getArea(name, ...args) {
+        const area = {
+            'circle' : () => this.getCircle(...args),
+            'rect' : () => this.getRect(...args),
+            'trapezoid' : () => this.getTrapezoid(...args)
+        }
+         area[name]();
+    }
+
+    addRecord(shape, area){
+        console.log(`${shape} : ${area}`);
+        this.executionSequence.push(`${shape} : ${area}`);
+    }
+
+
+    printExecutionSequence(){
+        console.log('●●●●●●●●●●계산수행순서●●●●●●●●●●●');
+        this.executionSequence.forEach((element, index) => console.log(`${index}번째. ${element}`));
+        console.log('●●●●●●●●●● 계산수행 끝 ●●●●●●●●●●●');
+    }
+
+};
+
+
+let test = new Polygon();
+test.getCircle(2);
+test.getCircle(2,4);
+test.getArea('circle',2);
+test.getArea('rect',2,3);
+test.getArea('trapezoid', 10,15,12);
+test.printExecutionSequence();
