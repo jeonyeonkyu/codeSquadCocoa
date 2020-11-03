44bc9244c73607ff78bbf07eeda1d364bb9f9951
yeonkyujeon <ehb6915@naver.com>
2020-11-04 00:12:18 +0900

    refactor: 포맷팅 맞춤, area인스턴스 변수로 수정

--- week1/polygon.js
+++ week1/polygon.js
@@ -1,53 +1,53 @@
 
 class Polygon {
-    constructor() {
-        this.executionSequence = [];
-    }
-    getCircle(radius, amount = radius) {
-        let sum = 0;
-        while (radius <= amount) {
-            sum += Math.pow(radius, 2) * Math.PI;
-            radius++;
-        }
-        this.addRecord('circle', sum);
-    }
+	constructor() {
+		this.executionSequence = [];
+		this.area = {
+			'circle': (...args) => this.getCircle(...args),
+			'rect': (...args) => this.getRect(...args),
+			'trapezoid': (...args) => this.getTrapezoid(...args)
+		}
+	}
+	getCircle(radius, amount = radius) {
+		let sum = 0;
+		while (radius <= amount) {
+			sum += Math.pow(radius, 2) * Math.PI;
+			radius++;
+		}
+		this.addRecord('circle', sum);
+	}
 
-    getRect(length, height){
-        this.addRecord('rect', length*height);
-    }
+	getRect(length, height) {
+		this.addRecord('rect', length * height);
+	}
 
-    getTrapezoid(upperSide, lowerSide, height){
-        this.addRecord('trapezoid', (upperSide + lowerSide) * height / 2);
-    }
+	getTrapezoid(upperSide, lowerSide, height) {
+		this.addRecord('trapezoid', (upperSide + lowerSide) * height / 2);
+	}
 
-    getArea(name, ...args) {
-        const area = {
-            'circle' : () => this.getCircle(...args),
-            'rect' : () => this.getRect(...args),
-            'trapezoid' : () => this.getTrapezoid(...args)
-        }
-         area[name]();
-    }
+	getArea(name, ...args) {
+		this.area[name](...args);
+	}
 
-    addRecord(shape, area){
-        console.log(`${shape} : ${area}`);
-        this.executionSequence.push(`${shape} : ${area}`);
-    }
+	addRecord(shape, area) {
+		console.log(`${shape} : ${area}`);
+		this.executionSequence.push(`${shape} : ${area}`);
+	}
 
 
-    printExecutionSequence(){
-        console.log('●●●●●●●●●●계산수행순서●●●●●●●●●●●');
-        this.executionSequence.forEach((element, index) => console.log(`${index+1}번째. ${element}`));
-        console.log('●●●●●●●●●● 계산수행 끝 ●●●●●●●●●●●');
-    }
+	printExecutionSequence() {
+		console.log('●●●●●●●●●●계산수행순서●●●●●●●●●●●');
+		this.executionSequence.forEach((element, index) => console.log(`${index + 1}번째. ${element}`));
+		console.log('●●●●●●●●●● 계산수행 끝 ●●●●●●●●●●●');
+	}
 
 };
 
 
 let test = new Polygon();
 test.getCircle(2);
-test.getCircle(2,4);
-test.getArea('circle',2);
-test.getArea('rect',2,3);
-test.getArea('trapezoid', 10,15,12);
+test.getCircle(2, 4);
+test.getArea('circle', 2);
+test.getArea('rect', 2, 3);
+test.getArea('trapezoid', 10, 15, 12);
 test.printExecutionSequence();
