df9895881355fb43492f531e0af609ab9634953c
yeonkyujeon <ehb6915@naver.com>
2020-11-05 03:01:08 +0900

    Refactor : 전체 메소드 수정

--- week1/numberConversionGame.js
+++ week1/numberConversionGame.js
@@ -1,43 +1,59 @@
 
 class Solution {
-    constructor() {
+    constructor({ baseNum, amount, peopleNum, order }) {
+        this.baseNum = baseNum;
+        this.amount = amount;
+        this.peopleNum = peopleNum;
+        this.order = order;
+        this.jinBubArray = [];
+        this.peopleArray = [];
+        this.result = [];
+        this.orderNum = [];
+        this.init();
 
     }
 
-    startGame(baseNum, amount, peopleNum, order) {
-        const allNumArr = this.getAllNumbers(baseNum, amount, peopleNum);
-        this.printResult1(allNumArr);
-        this.printResult2(this.getOrderNum(allNumArr, peopleNum, order));
+    init() {
+        this.jinBubArray = this.getAllNumbers(this.baseNum, this.amount, this.peopleNum);
+        this.peopleArray = this.getPeopleArray(this.peopleNum);
+        this.result = this.getResult(this.jinBubArray, this.peopleArray, this.peopleNum);
+        this.orderNum = this.getOrderNum(this.result, this.peopleNum, this.order);
     }
 
     getAllNumbers(baseNum, amount, peopleNum) {
-        let i = 0;
-        const result = [];
-        while (i < amount * peopleNum) {
-            const applicableNum = i.toString(baseNum).split('');
-            result.push(applicableNum);
-            i++;
-        }
-        return result.join().split(',');
+        return Array.from({ length: amount * peopleNum }, (_, index) => index.toString(baseNum)).join('').split('');
     }
-    getOrderNum(numArr, peopleNum, order) {
-        const gilDong = [];
-        numArr.forEach((element, index) => {
-            if (index % peopleNum === order - 1) {
-                gilDong.push(`순서 : ${index + 1}번째 , 값 :  ${element}`);
-            }
-        });
-        return gilDong;
+
+    getPeopleArray(peopleNum) {
+        return Array.from({ length: peopleNum }, (_, index) => String.fromCharCode(index + 65));
     }
-    printResult1(numArr) {
-        console.log(`문제 1번 : ${JSON.stringify(numArr)}`);
+
+    getResult(jinBubArr, peopleArr, peopleNum) {
+        return jinBubArr.map((element, index) => {
+            const key = `${peopleArr[index % peopleNum]}`;
+            return { [key]: element };
+        })
     }
-    printResult2(order) {
+
+    getOrderNum(result, peopleNum, order) {
+        return result.filter((_, index) => index % peopleNum === order - 1).map((element, index) =>
+            `${(index*peopleNum)+order}번째 ${JSON.stringify(element)}`);
+    }
+
+    run() {
+        this.printResult1(this.result);
+        this.printResult2(this.orderNum);
+    }
+
+    printResult1(result) {
+        console.log(`문제 1번 : `);
+        console.log(result);
+    }
+    printResult2(orderNum) {
         console.log(`문제 2번 : 길동이가 말해야 할 순서와 값`);
-        console.log(order);
+        console.log(orderNum);
     }
 }
 
-const test = new Solution();
-test.startGame(2,4,2,2);
-
+const test = new Solution({ baseNum: 2, amount: 4, peopleNum: 2, order: 2 });
+test.run();
