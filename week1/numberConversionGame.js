31a12ee0c1b6e336ca45c728a202dfe57b744b2e
yeonkyujeon <ehb6915@naver.com>
2020-11-04 14:25:26 +0900

    Fix  : 문제를 잘못해석하여 작성한 코드 수정

--- week1/numberConversionGame.js
+++ week1/numberConversionGame.js
@@ -1,8 +1,9 @@
+
 const solution = (baseNum, amount, peopleNum, gildongOrder) => {
     let i = 0;
     const result = [];
-    while (i.toString(baseNum).length < amount) {
-        const applicableNum = i.toString(2).split('');
+    while (i < amount * peopleNum) {
+        const applicableNum = i.toString(baseNum).split('');
         result.push(applicableNum);
         i++;
     }
@@ -10,13 +11,13 @@ const solution = (baseNum, amount, peopleNum, gildongOrder) => {
     console.log(`문제 1번 : ${JSON.stringify(printResult)}`);
 
     let gildong = [];
-    printResult.forEach((element,index) =>{
-        if(index % peopleNum === gildongOrder-1){
-            gildong.push(`순서 : ${index}번째 , 값 :  ${element} \n`);
+    printResult.forEach((element, index) => {
+        if (index % peopleNum === gildongOrder - 1) {
+            gildong.push(`순서 : ${index+1}번째 , 값 :  ${element}`);
         }
     });
-
-    console.log(`문제 2번 : 길동이 ${gildong}`);
+    console.log(`문제 2번 : 길동이`)
+    console.log(gildong);
 };
 
-solution(2, 4, 3, 3);
+solution(2, 4, 2, 2);
