463ebdc2c03d1551cb72cb5e35b37d933c02fdb8
yeonkyujeon <ehb6915@naver.com>
2020-11-04 20:30:03 +0900

    Refactor : 인덱스에 접근하여 원본이 수정된 for문을 문자열에 값을 따로 할당하여 원본이 수정되지 않게 수정

--- week1/arrayFilter.js
+++ week1/arrayFilter.js
@@ -4,13 +4,15 @@ const regExpNum = /[0-9]/g;
 //for/while문을 사용한 버전 만들기
 const filterId_v1 = (peoples) => {
     const specialCharRemoveArr = [];
-
+    let result = '';
     for (let i = 0; i < peoples.length; i++) {
-        if (regExpNum.test(peoples[i])) {
-            peoples[i] = peoples[i].replace(regExpNum, '');
-        }
         if (!regExpSpecial.test(peoples[i])) {
-            specialCharRemoveArr.push(peoples[i]);
+            if (regExpNum.test(peoples[i])) {
+                result = peoples[i].replace(regExpNum, '');
+            } else {
+                result = peoples[i];
+            }
+            specialCharRemoveArr.push(result);
         }
     }
     return specialCharRemoveArr;
@@ -22,14 +24,8 @@ const filterId_v2 = (peoples) => {
 }
 
 
-
-
 const peoples = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
 console.log(filterId_v1(peoples));
 const peoples2 = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
 console.log(filterId_v2(peoples2));
 
-//for문을 사용하여 index에 접근한 filterId_v1()는 replace함수로 인해 peoples값이 변경 되었다.
-console.log(peoples);
-//filter와 map을 사용한 filterId_v2()는 index에 접근하지 않아 값이 그대로이다.
-console.log(peoples2);
