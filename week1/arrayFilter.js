73f22e0949230dcfd60a2a34b375f8c89ca856be
yeonkyujeon <ehb6915@naver.com>
2020-11-04 16:24:52 +0900

    update : forEach,filter, map등의 고차함수를 사용한 버전 만들기  버전2 함수 생성

--- week1/arrayFilter.js
+++ week1/arrayFilter.js
@@ -1,5 +1,5 @@
 
-
+//for/while문을 사용한 버전 만들기
 const filterId_v1 = (peoples) => {
     const regExpSpecial = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi;
     const regExpNum = /[0-9]/g;
@@ -16,6 +16,24 @@ const filterId_v1 = (peoples) => {
     return specialCharRemoveArr;
 }
 
+//forEach,filter, map등의 고차함수를 사용한 버전 만들기
+const filterId_v2 = (peoples) => {
+    const regExpSpecial = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi;
+    const regExpNum = /[0-9]/g;
+    const specialCharRemoveArr = [];
+
+    return peoples.filter((element) => !regExpSpecial.test(element)).map((element) => element.replace(regExpNum,''));
+}
+
+
+
 
 const peoples = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
 console.log(filterId_v1(peoples));
+const peoples2 = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
+console.log(filterId_v2(peoples2));
+
+//for문을 사용하여 index에 접근한 filterId_v1()는 replace함수로 인해 peoples값이 변경 되었다.
+console.log(peoples);
+//filter와 map을 사용한 filterId_v2()는 index에 접근하지 않아 값이 그대로이다.
+console.log(peoples2);
