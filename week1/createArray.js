eb5afbeea6df71125f53e8c4cca5736a3ed554ef
yeonkyujeon <ehb6915@naver.com>
2020-11-04 20:47:17 +0900

    Refactor : 재귀함수 이용하여 해결 및 ES6문법 class형식으로 만들기

--- week1/createArray.js
+++ week1/createArray.js
@@ -1,12 +1,22 @@
 //숫자타입으로만 구성된 요소를 뽑아 배열만들기
-
-const filteringNumberTypeToArr = (obj) => {
-    const regExpNum = /[0-9]/g;
-    const result = [];
-    for(let key in obj){
-        console.log(`key = ${key}, obj[key] = ${JSON.stringify(obj[key])}`);
+class CreateArray {
+    constructor() {
+        this.arr = [];
+    }
+    filteringNumberTypeToArr(obj) {
+        Object.keys(obj).forEach((key) => {
+            if (typeof obj[key] === 'object') {
+                this.filteringNumberTypeToArr(obj[key]);
+            } else {
+                if (typeof obj[key] === 'number') {
+                    this.arr.push(key);
+                }
+            }
+        });
+        return this.arr;
     }
 }
+
 
 
 
@@ -40,4 +50,5 @@ const data = {
 }
 
 
-filteringNumberTypeToArr(data);
+const createArray = new CreateArray();
+console.log(createArray.filteringNumberTypeToArr(data));
