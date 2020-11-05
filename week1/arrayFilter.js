7079f5c3e995c494a7510da69793d5787152967c
yeonkyujeon <ehb6915@naver.com>
2020-11-05 23:02:15 +0900

    Refactor : if문 삼항연산자로 수정 및 탭 간격 수정

--- week1/arrayFilter.js
+++ week1/arrayFilter.js
@@ -2,30 +2,28 @@ const regExpSpecial = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi;
 const regExpNum = /[0-9]/g;
 
 //for/while문을 사용한 버전 만들기
-const filterId_v1 = (peoples) => {
-    const specialCharRemoveArr = [];
-    let result = '';
-    for (let i = 0; i < peoples.length; i++) {
-        if (!regExpSpecial.test(peoples[i])) {
-            if (regExpNum.test(peoples[i])) {
-                result = peoples[i].replace(regExpNum, '');
-            } else {
-                result = peoples[i];
-            }
-            specialCharRemoveArr.push(result);
-        }
+const filterId_v1 = (people) => {
+  const specialCharRemoveArr = [];
+  let result = '';
+  for (let i = 0; i < people.length; i++) {
+    if (!regExpSpecial.test(people[i])) {
+      result = regExpNum.test(people[i]) ? people[i].replace(regExpNum, '') : people[i];
+      specialCharRemoveArr.push(result);
     }
-    return specialCharRemoveArr;
+  }
+  return specialCharRemoveArr;
 }
 
 //forEach,filter, map등의 고차함수를 사용한 버전 만들기
-const filterId_v2 = (peoples) => {
-    return peoples.filter((element) => !regExpSpecial.test(element)).map((element) => element.replace(regExpNum, ''));
+const filterId_v2 = (people) => {
+  return people
+    .filter((element) => !regExpSpecial.test(element))
+    .map((element) => element.replace(regExpNum, ''));
 }
 
 
-const peoples = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
-console.log(filterId_v1(peoples));
-const peoples2 = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
-console.log(filterId_v2(peoples2));
+const people = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
+console.log(filterId_v1(people));
+const people2 = ["crong!@#", "honux5", "sarah#", "hea3d", "zello", "5lucas"];
+console.log(filterId_v2(people2));
 
