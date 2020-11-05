959bf68ffa3faa503f30574aeda08432b6fdaed7
yeonkyujeon <ehb6915@naver.com>
2020-11-05 17:47:47 +0900

    Refactor : 재귀함수 사용하여 factorial함수 구현하는 것으로 수정

--- week1/factorial.js
+++ week1/factorial.js
@@ -1,12 +1,13 @@
 
+//1. factorial 함수
+//임의의 숫자(m)를 입력받아 1부터 m까지의 factorial 값을 배열로 담아서 반환하는 함수 만들기.
 const factorial = (num) => {
-    const result = [];
-    let sum = 1;
-    for(let i = 1; i<=num; i++){
-        result.push(i*sum);
-        sum *= i;
-    }
-    return result;
+	if (num <= 1) return 1;
+	return num * factorial(num - 1);
 }
 
-console.log(factorial(4));
+const calculate = (num) => {
+	return Array.from({ length: num }, (_, i) => factorial(i + 1));
+}
+
+console.log(calculate(4));
