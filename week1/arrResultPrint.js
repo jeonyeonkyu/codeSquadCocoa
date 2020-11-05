6a20f1571e5dbe369b70fc5b69f2185ebee77fe2
yeonkyujeon <ehb6915@naver.com>
2020-11-06 00:36:17 +0900

    Refactor : 데이터가 담겨있는 객체 파일을 exports, require(),를 사용하여 분리

--- week1/arrResultPrint.js
+++ week1/arrResultPrint.js
@@ -1,5 +1,6 @@
-
 // type이 sk인, name으로 구성된 배열만 출력해본다.
+const skNameO = require('./skNameO.js');
+
 class ArrResultPrint {
 	constructor() {
 		this.arr = [];
@@ -18,88 +19,5 @@ class ArrResultPrint {
 	}
 }
 
-
-
-const data = [{
-	"id": 1,
-	"name": "Yong",
-	"phone": "010-0000-0000",
-	"type": "sk",
-	"childnode": [{
-		"id": 11,
-		"name": "echo",
-		"phone": "010-0000-1111",
-		"type": "kt",
-		"childnode": [{
-			"id": 115,
-			"name": "hary",
-			"phone": "211-1111-0000",
-			"type": "sk",
-			"childnode": [{
-				"id": 1159,
-				"name": "pobi",
-				"phone": "010-444-000",
-				"type": "kt",
-				"childnode": [{
-					"id": 11592,
-					"name": "cherry",
-					"phone": "111-222-0000",
-					"type": "lg",
-					"childnode": []
-				},
-				{
-					"id": 11595,
-					"name": "solvin",
-					"phone": "010-000-3333",
-					"type": "sk",
-					"childnode": []
-				}
-				]
-			}]
-		},
-		{
-			"id": 116,
-			"name": "kim",
-			"phone": "444-111-0200",
-			"type": "kt",
-			"childnode": [{
-				"id": 1168,
-				"name": "hani",
-				"phone": "010-222-0000",
-				"type": "sk",
-				"childnode": [{
-					"id": 11689,
-					"name": "ho",
-					"phone": "010-000-0000",
-					"type": "kt",
-					"childnode": [{
-						"id": 116890,
-						"name": "wonsuk",
-						"phone": "010-000-0000",
-						"type": "kt",
-						"childnode": []
-					},
-					{
-						"id": 1168901,
-						"name": "chulsu",
-						"phone": "010-0000-0000",
-						"type": "sk",
-						"childnode": []
-					}
-					]
-				}]
-			}]
-		},
-		{
-			"id": 117,
-			"name": "hong",
-			"phone": "010-0000-0000",
-			"type": "lg",
-			"childnode": []
-		}
-		]
-	}]
-}]
-
 const result = new ArrResultPrint();
-console.log(result.nameIsSkFilterArr(data));
+console.log(result.nameIsSkFilterArr(skNameO));
--- week1/createArray.js
+++ week1/createArray.js
@@ -1,4 +1,6 @@
 //숫자타입으로만 구성된 요소를 뽑아 배열만들기
+const o = require('./o.js')
+
 class CreateArray {
   constructor() {
     this.arr = [];
@@ -17,38 +19,5 @@ class CreateArray {
   }
 }
 
-
-
-
-
-
-const data = {
-  "debug": "on",
-  "window": {
-    "title": "Sample Konfabulator Widget",
-    "name": "main_window",
-    "width": 500,
-    "height": 500
-  },
-  "image": {
-    "src": "Images/Sun.png",
-    "name": "sun1",
-    "hOffset": 250,
-    "vOffset": 250,
-    "alignment": "center"
-  },
-  "text": {
-    "data": "Click Here",
-    "size": 36,
-    "style": "bold",
-    "name": "text1",
-    "hOffset": 250,
-    "vOffset": 100,
-    "alignment": "center",
-    "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
-  }
-}
-
-
 const createArray = new CreateArray();
-console.log(createArray.filteringNumberTypeToArr(data));
+console.log(createArray.filteringNumberTypeToArr(o));
