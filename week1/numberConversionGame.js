9fdc3b8709c99f6380ea47254e97b76cadc032df
yeonkyujeon <ehb6915@naver.com>
2020-11-05 14:52:00 +0900

    Refactor : run()함수 안의 printResult1()의 매개변수를 없애야 따로따로 1,2를 호출 할 수 있을거 같아 수정

--- week1/numberConversionGame.js
+++ week1/numberConversionGame.js
@@ -37,23 +37,26 @@ class Solution {
 
     getOrderNum(result, peopleNum, order) {
         return result.filter((_, index) => index % peopleNum === order - 1).map((element, index) =>
-            `${(index*peopleNum)+order}번째 ${JSON.stringify(element)}`);
+            `${(index * peopleNum) + order}번째 ${JSON.stringify(element)}`);
     }
 
     run() {
-        this.printResult1(this.result);
-        this.printResult2(this.orderNum);
+        this.printResult1();
+        this.printResult2();
     }
 
-    printResult1(result) {
+    printResult1() {
         console.log(`문제 1번 : `);
-        console.log(result);
+        console.log(this.result);
     }
-    printResult2(orderNum) {
+    printResult2() {
         console.log(`문제 2번 : 길동이가 말해야 할 순서와 값`);
-        console.log(orderNum);
+        console.log(this.orderNum);
     }
 }
 
-const test = new Solution({ baseNum: 2, amount: 4, peopleNum: 2, order: 2 });
+const test = new Solution({ baseNum: 2, amount: 4, peopleNum: 3, order: 2 });
 test.run();
+
+// const test2 = new Solution({ baseNum: 2, amount: 4, peopleNum: 2});
+// test2.run();
