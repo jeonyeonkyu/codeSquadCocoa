1eaaf94c777c7e52272a31c94f9673b2544e756c
yeonkyujeon <ehb6915@naver.com>
2020-11-06 15:51:03 +0900

    Implement : 11654번 문제 구현

---
+++ week1/algorithm/BEAKJOON_ex_11654.js
@@ -0,0 +1,6 @@
+// https://www.acmicpc.net/problem/11654
+// 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString();
+
+console.log(input.charCodeAt(0));
