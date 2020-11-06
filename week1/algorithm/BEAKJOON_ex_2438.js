1c1c19510bba990216b070b18cd13bac50e8967a
yeonkyujeon <ehb6915@naver.com>
2020-11-06 12:16:43 +0900

    Implement : 2438문제 구현

---
+++ week1/algorithm/BEAKJOON_ex_2438.js
@@ -0,0 +1,19 @@
+//https://www.acmicpc.net/problem/2438
+//첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin');
+// const a = parseInt(input);
+const solution = (A) => {
+  Array.from({ length: A }, (_, input) => input+1)
+  .forEach((element,_) => {
+    const star = "*";
+    let result = '';
+    for(let i = 0; i<element;i++){
+      result += star;
+    }
+    console.log(result);
+  })
+};
+
+const a = 3
+solution(a);
