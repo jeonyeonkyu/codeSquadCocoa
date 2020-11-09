107a6e789d2f9948cc7ad355a1964a08110b8a26
yeonkyujeon <ehb6915@naver.com>
2020-11-09 15:45:36 +0900

    Add : Hashmap 요구사항 작성하여 뼈대만들기

---
+++ week2/myHashMap.js
@@ -0,0 +1,51 @@
+//linkedList 구현하지 못하여 라이브러리 사용 
+//linkedList도 다음에 구현하기
+const LinkedList = require('linked-list');
+
+//해시맵처럼 동작하는 코드를 구현하기
+//문자열 키와 문자열 값을 저장하는 해시맵 라이브러리를 구현한다.
+// ES Classes를 활용한 방법으로 구현한다.
+
+// 고유한 Hash 함수를 정한다.
+class MyHashMap {
+  constructor() {
+  }
+  //put(String key, String value) 키-값을 추가한다.
+  put(key, value) {
+
+  }
+
+  // remove(String key) 해당 키에 있는 값을 삭제한다.
+  remove(key) {
+
+  }
+  // containsKey(String) 해당 키가 존재하는지 판단해서 Bool 결과를 리턴한다.
+  containsKey(str) {
+
+  }
+
+  // get(String) 해당 키와 매치되는 값을 찾아서 리턴한다.
+  get(str) {
+
+  }
+  // isEmpty() 비어있는 맵인지 Bool 결과를 리턴한다.
+  inEmpty() {
+
+  }
+  // keys() 전체 키 목록을 [String] 배열로 리턴한다.
+  keys() {
+
+  }
+  // replace(String key, String value) 키-값으로 기존 값을 대체한다.
+  replace(key, value) {
+
+  }
+  // size() 전체 아이템 개수를 리턴한다.
+  size() {
+
+  }
+  // clear() 전체 맵을 초기화한다.
+  clear() {
+
+  }
+}
