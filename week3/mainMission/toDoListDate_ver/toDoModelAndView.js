1668f81b193e44aaa239a4e572edff4ae68d2d5f
yeonkyujeon <ehb6915@naver.com>
2020-11-19 22:28:08 +0900

    Refactor : showListByDate() 함수 수정 forEach -> map 으로 수정

--- week3/mainMission/toDoListDate_ver/toDoModelAndView.js
+++ week3/mainMission/toDoListDate_ver/toDoModelAndView.js
@@ -154,19 +154,19 @@ class View {
       editButton.replaceWith(editOkButton);
     }
   }
-////////////////////////////////
-  togglePage(){
+
+  togglePage() {
     const buttonName = document.querySelector('button').className;
-    if(buttonName === 'view_by_date'){
+    if (buttonName === 'view_by_date') {
       this.showList(this.todoModel.getTodoModel());
-    }else if(buttonName === 'view_by_writing_order'){
+    } else if (buttonName === 'view_by_writing_order') {
       this.todoModel.splitModelByDate();
       this.showListByDate();
     }
   }
 
 
-  createEditOkButton(){
+  createEditOkButton() {
     const editOkButton = document.createElement('Button');
     editOkButton.className = 'edit_ok_button';
     editOkButton.innerHTML = 'Ok'
@@ -189,29 +189,27 @@ class View {
 
   showListByDate() {
     const sortedDateArray = this.todoModel.getArraySortedByDate();
-    let template = `<div class="view_by_date_page">`;
-    sortedDateArray.forEach(element => {
-      template += `<div class="box_by_date">
-                   <div class="date_of_writing">${element[0]}</div>`
-      element[1].forEach(({ elementId, checked, item, date }) => {
-        template +=
-          `<div class="item_box" id = ${elementId}>
-            <div>
-              <input class="todo_checkbox" type="checkbox" ${checked ? 'checked' : ''}>
-              <button class="edit_button">edit</button>
-            </div>
-            <li class="written" style="text-decoration: ${checked ? 'line-through' : 'none'}">${item}</li>
-            <svg class="trash_basket" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-              enable-background="new 0 0 32 32" id="Glyph" version="1.1" viewBox="0 0 32 32" xml:space="preserve">
-             <path class="trash_basket_path" d="M6,12v15c0,1.654,1.346,3,3,3h14c1.654,0,3-1.346,3-3V12H6z M12,25c0,0.552-0.448,1-1,1s-1-0.448-1-1v-9  c0-0.552,0.448-1,1-1s1,0.448,1,1V25z M17,25c0,0.552-0.448,1-1,1s-1-0.448-1-1v-9c0-0.552,0.448-1,1-1s1,0.448,1,1V25z M22,25  c0,0.552-0.448,1-1,1s-1-0.448-1-1v-9c0-0.552,0.448-1,1-1s1,0.448,1,1V25z"
-              id="XMLID_237_" />
-             <path class="trash_basket_path" d="M27,6h-6V5c0-1.654-1.346-3-3-3h-4c-1.654,0-3,1.346-3,3v1H5C3.897,6,3,6.897,3,8v1c0,0.552,0.448,1,1,1h24  c0.552,0,1-0.448,1-1V8C29,6.897,28.103,6,27,6z M13,5c0-0.551,0.449-1,1-1h4c0.551,0,1,0.449,1,1v1h-6V5z"
-              id="XMLID_243_" /></svg>
-         </div>`
-      })
-      template += `</div>`
-    })
-    template += `</div>`
+    let template = `<div class="view_by_date_page">`
+    template += sortedDateArray.map(([first, second]) => `
+      <div class="box_by_date">
+      <div class="date_of_writing">${first}</div>
+      ${second.map(({ elementId, checked, item, date }) =>
+      `<div class="item_box" id = ${elementId}>
+          <div>
+            <input class="todo_checkbox" type="checkbox" ${checked ? 'checked' : ''}>
+            <button class="edit_button">edit</button>
+          </div>
+          <li class="written" style="text-decoration: ${checked ? 'line-through' : 'none'}">${item}</li>
+          <svg class="trash_basket" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            enable-background="new 0 0 32 32" id="Glyph" version="1.1" viewBox="0 0 32 32" xml:space="preserve">
+          <path class="trash_basket_path" d="M6,12v15c0,1.654,1.346,3,3,3h14c1.654,0,3-1.346,3-3V12H6z M12,25c0,0.552-0.448,1-1,1s-1-0.448-1-1v-9  c0-0.552,0.448-1,1-1s1,0.448,1,1V25z M17,25c0,0.552-0.448,1-1,1s-1-0.448-1-1v-9c0-0.552,0.448-1,1-1s1,0.448,1,1V25z M22,25  c0,0.552-0.448,1-1,1s-1-0.448-1-1v-9c0-0.552,0.448-1,1-1s1,0.448,1,1V25z"
+            id="XMLID_237_" />
+          <path class="trash_basket_path" d="M27,6h-6V5c0-1.654-1.346-3-3-3h-4c-1.654,0-3,1.346-3,3v1H5C3.897,6,3,6.897,3,8v1c0,0.552,0.448,1,1,1h24  c0.552,0,1-0.448,1-1V8C29,6.897,28.103,6,27,6z M13,5c0-0.551,0.449-1,1-1h4c0.551,0,1,0.449,1,1v1h-6V5z"
+            id="XMLID_243_" /></svg>
+        </div>`
+    )}
+      </div>
+    `) + `</div>`
     this.article.innerHTML = template;
   }
 
@@ -227,7 +225,6 @@ class View {
     event.target.replaceWith(this.viewByDateButton);
     this.togglePage();
   }
-
 }
 
 document.addEventListener('DOMContentLoaded', () => {
