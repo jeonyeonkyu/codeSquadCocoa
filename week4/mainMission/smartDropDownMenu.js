0c7601fa33c454b48c22fc84b7f40bd8a01fb1d7
yeonkyujeon <ehb6915@naver.com>
2020-11-23 21:58:37 +0900

    Feat : smartDropDownMenu 구현

---
+++ week4/mainMission/smartDropDownMenu.js
@@ -0,0 +1,97 @@
+const _ = {
+  $(selector, base = document) {
+    return base.querySelector(selector)
+  }
+}
+
+class Model {
+  constructor() {
+    this.fruitObj = {};
+  }
+
+  addData(fruitType) {
+    if (!this.fruitObj.hasOwnProperty(fruitType)) {
+      this.fruitObj[fruitType] = 1;
+    } else {
+      this.fruitObj[fruitType]++;
+    }
+  }
+}
+
+class FruitView {
+  constructor() { }
+
+  init({ listTitle, fruitList }) {
+    this.listTitle = listTitle;
+    this.fruitList = fruitList;
+    this.contents = null;
+    this.initEvent();
+  }
+
+  initEvent() {
+    this.listTitle.addEventListener('mouseenter', this.mouseEnterTitleHandler);
+    this.listTitle.addEventListener('mouseleave', this.mouseLeaveTitleHandler);
+  }
+
+  mouseEnterTitleHandler = () => {
+    this.contents = window.setTimeout(() => this.showList(), 1000);
+  }
+
+  mouseLeaveTitleHandler = () => {
+    window.clearTimeout(this.contents);
+  }
+
+  showList = () => {
+    this.fruitList.style.display = 'block';
+  }
+}
+
+class DataView {
+  constructor() { }
+
+  init({ model, fruitDataList, fruitList }) {
+    this.model = model;
+    this.fruitDataList = fruitDataList;
+    this.fruitList = fruitList;
+    this.movementTimer = null;
+    this.initEvent();
+  }
+
+  initEvent() {
+    this.fruitList.addEventListener('mousemove', this.mouseMoveDataHandler);
+  }
+
+  mouseMoveDataHandler = (event) => {
+    window.clearTimeout(this.movementTimer);
+    this.movementTimer = window.setTimeout(() => this.moveToAdd(event), 500);
+  }
+
+  moveToAdd = (event) => {
+    if (event.target.tagName === 'LI') {
+      this.model.addData(event.target.innerHTML);
+      this.showData();
+    }
+  }
+
+  showData = () => {
+    const template = Object.keys(this.model.fruitObj).map(ele =>
+      `<li> ${ele} : ${this.model.fruitObj[ele]} </li>`
+    ).join('');
+    this.fruitDataList.innerHTML = template;
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const listTitle = _.$('.list_title');
+  const fruitList = _.$('.fruit_list');
+  const fruitDataList = _.$('.fruit_data_list');
+
+  const model = new Model();
+
+  const fruitView = new FruitView();
+  const dataView = new DataView();
+  fruitView.init({ listTitle, fruitList });
+  dataView.init({ model, fruitDataList, fruitList })
+
+})
