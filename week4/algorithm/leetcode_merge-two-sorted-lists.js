e3a178fd85925ca11b1dc8e4a4cd07569f262362
yeonkyujeon <ehb6915@naver.com>
2020-11-28 17:12:38 +0900

    Fix : 정렬

--- week4/algorithm/leetcode_merge-two-sorted-lists.js
+++ week4/algorithm/leetcode_merge-two-sorted-lists.js
@@ -11,26 +11,26 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
-    
+var mergeTwoLists = function (l1, l2) {
+
   let result = new ListNode();
   let rr = result;
-  while(l1 !== null && l2 !== null ){
-       if(l1.val < l2.val){
-           result.next = new ListNode(l1.val);
-           l1 = l1.next;
-       }else{
-           result.next = new ListNode(l2.val);
-           l2 = l2.next;
-       }
-      result = result.next;
-   }
-   
-   if(l1 !== null){
-       result.next = l1;
-   }else if(l2 !== null){
-       result.next = l2
-   }
-   
+  while (l1 !== null && l2 !== null) {
+    if (l1.val < l2.val) {
+      result.next = new ListNode(l1.val);
+      l1 = l1.next;
+    } else {
+      result.next = new ListNode(l2.val);
+      l2 = l2.next;
+    }
+    result = result.next;
+  }
+
+  if (l1 !== null) {
+    result.next = l1;
+  } else if (l2 !== null) {
+    result.next = l2
+  }
+
   return rr.next
 };
