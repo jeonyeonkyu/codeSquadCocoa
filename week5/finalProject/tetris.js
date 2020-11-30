36966ec6a1ef7891ac9d24fd2b958870b1ee0bfb
yeonkyujeon <ehb6915@naver.com>
2020-11-30 20:19:29 +0900

    Fix : 블럭 회전을 구현하기 위한 모델 3차원 배열로 하드코딩

--- week5/finalProject/tetris.js
+++ week5/finalProject/tetris.js
@@ -4,23 +4,83 @@ class TetrisModel {
       Array.from({ length: 10 }, () => 0));
     this.shape = [
       { name: 0, color: 'white' },
-      { name: 1, pattern: [[1, 1], [1, 1]], color: 'yellow' },
-      { name: 2, pattern: [[0, 2, 0], [2, 2, 2]], color: 'pink' },
-      { name: 3, pattern: [[3, 3, 0], [0, 3, 3]], color: 'greenyellow' },
-      { name: 4, pattern: [[0, 4, 4], [4, 4, 0]], color: 'coral' },
-      { name: 5, pattern: [[5, 5, 5], [0, 0, 5]], color: 'purple' },
-      { name: 6, pattern: [[6, 6, 6], [6, 0, 0]], color: 'blue' },
-      { name: 7, pattern: [[7, 7, 7, 7]], color: 'red' }
+      { name: 1, pattern: [[[1, 1], [1, 1]]], color: 'yellow' },
+      {
+        name: 2,
+        pattern:
+          [[[0, 2, 0], [2, 2, 2], [0, 0, 0]],
+          [[0, 2, 0], [0, 2, 2], [0, 2, 0]],
+          [[0, 0, 0], [2, 2, 2], [0, 2, 0]],
+          [[0, 2, 0], [2, 2, 0], [0, 2, 0]]],
+        color: 'pink'
+      },
+      {
+        name: 3,
+        pattern:
+          [[[3, 3, 0], [0, 3, 3], [0, 0, 0]],
+          [[0, 0, 3], [0, 3, 3], [0, 3, 0]],
+          [[0, 0, 0], [3, 3, 0], [0, 3, 3]],
+          [[0, 3, 0], [3, 3, 0], [3, 0, 0]]],
+        color: 'greenyellow'
+      },
+      {
+        name: 4,
+        pattern:
+          [[[0, 4, 4], [4, 4, 0], [0, 0, 0]],
+          [[0, 4, 0], [0, 4, 4], [0, 0, 4]],
+          [[0, 0, 0], [0, 4, 4], [4, 4, 0]],
+          [[4, 0, 0], [4, 4, 0], [0, 4, 0]]],
+        color: 'coral'
+      },
+      {
+        name: 5,
+        pattern:
+          [[[5, 5, 5], [0, 0, 5], [0, 0, 0]]
+          [[0, 0, 5], [0, 0, 5], [0, 5, 5]],
+          [[0, 0, 0], [5, 0, 0], [5, 5, 5]],
+          [[5, 5, 0], [5, 0, 0], [5, 0, 0]]],
+        color: 'purple'
+      },
+      {
+        name: 6,
+        pattern:
+          [[[6, 6, 6], [6, 0, 0], [0, 0, 0]],
+          [[0, 6, 6], [0, 0, 6], [0, 0, 6]],
+          [[0, 0, 0], [0, 0, 6], [6, 6, 6]],
+          [[6, 0, 0], [6, 0, 0], [6, 6, 0]]],
+        color: 'blue'
+      },
+      {
+        name: 7,
+        pattern:
+          [[[0, 0, 0, 0], [7, 7, 7, 7], [0, 0, 0, 0], [0, 0, 0, 0]],
+          [[0, 0, 7, 0], [0, 0, 7, 0], [0, 0, 7, 0], [0, 0, 7, 0]]],
+        color: 'red'
+      }
     ]
   }
 
+  run() {
+    const block = this.createBlock();
+    this.puttingInModel(block);
+    this.goingDownBlock(block);
+  }
+
   createBlock() {
-    const block = this.shape[Math.ceil(Math.random() * 7)].pattern;
+    const block = this.shape[Math.ceil(Math.random() * 7)].pattern[0];
+    return block;
+  }
+
+  puttingInModel(block) {
     block.forEach((tr, i) => {
       tr.forEach((td, j) => {
-        this.model[i][j+3] = td;  
+        this.model[i][j + 3] = td;
       })
     })
+  }
+
+  goingDownBlock(block) {
+    
   }
 
   getModel() {
@@ -38,8 +98,6 @@ class RenderView {
     this.renderingFromModel();
   }
 
-
-
   renderingFromModel() {
     const template = `<table>` + this.tetrisModel.getModel().map((tr, i) =>
       `<tr>
@@ -56,46 +114,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const gameView = document.querySelector('.game_view');
   const renderView = new RenderView({ tetrisModel, gameView });
-  tetrisModel.createBlock();
+  tetrisModel.run();
   renderView.run();
 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function createView() {
-//   let gameView = '<table>'
-//   for (let i = 0; i < 20; i++) {
-//     gameView += '<tr>'
-//     for (let j = 0; j < 10; j++) {
-//       gameView += '<td>0</td>'
-//     }
-//     gameView += '</tr>'
-//   }
-//   gameView += '</table>'
-//   document.querySelector('.game_view').innerHTML = gameView;
-// }
-// document.addEventListener('DOMContentLoaded', () => {
-//   createView();
-// })
